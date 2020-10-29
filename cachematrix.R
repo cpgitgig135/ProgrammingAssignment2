47d1a4421b2980618a62e2da7c102bdeadf76e4e
HiEveryOne3k <68483672+cpgitgig135@users.noreply.github.com>
2020-10-29 17:00:01 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,75 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Code to cache the inverse of a squared matrix
+# Please copy the entire contents and paste them into R to run and see how it works
+# Please press the "Enter" key additional times to see the additional results of the tests at the end
 
-## Write a short comment describing this function
+# makeCacheMatrix creates a special "matrix" object that
+# can cache its inverse:
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      
+        m = NULL
+        set = function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get = function() x
+        setinv = function(inverse) m <<- inverse 
+        getinv = function() m
+        list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
-
-## Write a short comment describing this function
+# cacheSolve computes the inverse of the special "matrix"
+# returned by makeCacheMatrix above.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m = x$getinv()
+        
+        # if the inverse was already calculated then output a message:
+        if (!is.null(m)){
+                # get it from the cache 
+                message("getting cached data")
+                return(m)
+        }
+        
+        # otherwise, calculate the inverse: 
+        data = x$get()
+        m = solve(data, ...) # the solve function of R to produce the inverse
+        
+        # set the value of the inverse
+        x$setinv(m)
+        return(m)
 }
+
+# Function named testx that measures the time the functions above take
+# to show that there was time saved:
+
+timer = function(mat){
+               
+        temp = makeCacheMatrix(mat)
+        
+        start.time = Sys.time()
+        cacheSolve(temp)
+        dur = Sys.time() - start.time
+        print(dur)
+        
+        start.time = Sys.time()
+        cacheSolve(temp)
+        dur = Sys.time() - start.time
+        print(dur)
+}
+
+# Tests for the code above:
+
+test_1 <- makeCacheMatrix(matrix(1:4,2,2))
+test_1$get()
+test_1$getinv()
+cacheSolve(test_1)
+
+timer(matrix(1:4,2,2))
+
+test_1 <- makeCacheMatrix(matrix(c(3, 9, -1, 4, 2, 6, 5, 7, 9), nrow = 3))
+test_1$get()
+test_1$getinv()
+cacheSolve(test_1)
+
+timer(matrix(c(3, 9, -1, 4, 2, 6, 5, 7, 9), nrow = 3))
